e55078f371657ac7a9cb888e3e7004a5b9c10299
TNtube <stanis.kapus@gmail.com>
2020-11-24 15:46:50 -0300

    Fix bugs where faces were badly displayed

--- main.py
+++ main.py
@@ -33,34 +33,43 @@ def rot_matrix(a, b, t):
 
 
 class Object:
-    def __init__(self, vertices, edges, length):
+    def __init__(self, vertices, edges, length, size):
         self.vertices = vertices
         self.edges = edges
         self.rotation = [3, 3, 0]
-        self.len = length
+        self.movement = [size[0], size[1], length]
 
     def rotate(self, axe, o):
         self.rotation[axe] += o
 
     def polygon(self):
         location = mul_matrix(self.vertices, rot_matrix(*self.rotation))
-        return {color: (location[v1], location[v2], location[v3], location[v4])
-                for color, (v1, v2, v3, v4) in self.edges.items()}
+        return [(color, (location[v1], location[v2], location[v3], location[v4]))
+                for color, (v1, v2, v3, v4) in self.edges]
 
 
-def point(size, vec, shape):
-    return [round(shape.len / 2 * coordinate + frame / 2) for coordinate, frame in zip(vec, size)]
+def point(vec, shape):
+    return [round(shape.movement[2] / 2 * coordinate + frame / 2)
+            for coordinate, frame in zip(vec, shape.movement[:-1])]
 
 
-def draw_shape(screen, shape, size):
-    pol = shape.polygon()
-    for col, points in pol.items():
-        pygame.draw.polygon(screen, col, [*(point(size, dot, shape)for dot in points)])
+def average_z(dots):
+    return sum(dot[2] for dot in dots)
+
+
+def draw_shape(screen, shapes):
+    pol = []
+    for shape in shapes:
+        pol.extend(shape.polygon())
+    pol.sort(key=lambda p: average_z(p[1]))
+
+    for col, points in pol:
+        pygame.draw.polygon(screen, col, [*(point(dot, shapes[0])for dot in points)])
 
 
 def main(screen):
     clock = pygame.time.Clock()
-    size = screen.get_width(), screen.get_height()
+    size = [screen.get_width(), screen.get_height()]
     vertices = [(-1, -1, -1), (-1, -1, 1),
                 (-1, 1, -1), (-1, 1, 1),
                 (1, -1, -1), (1, -1, 1),
@@ -68,12 +77,11 @@ def main(screen):
     lines = [(0, 1), (0, 2), (2, 3), (1, 3),
              (4, 5), (4, 6), (6, 7), (5, 7),
              (0, 4), (1, 5), (2, 6), (3, 7)]
-    choice(colors)
 
     def pol():
-        return {choice(colors): (0, 1, 3, 2), choice(colors): (0, 1, 5, 4),
-                choice(colors): (4, 5, 7, 6), choice(colors): (2, 3, 7, 6),
-                choice(colors): (0, 4, 6, 2), choice(colors): (1, 5, 7, 3)}
+        return [(choice(colors), (0, 1, 3, 2)), (choice(colors), (0, 1, 5, 4)),
+                (choice(colors), (4, 5, 7, 6)), (choice(colors), (2, 3, 7, 6)),
+                (choice(colors), (0, 4, 6, 2)), (choice(colors), (1, 5, 7, 3))]
 
     cubes = []
     for i in range(-1, 2):
@@ -81,7 +89,7 @@ def main(screen):
             for k in range(-1, 2):
                 vert = [(pos[0] + i * 2, pos[1] + j * 2, pos[2] + k * 2) for pos in vertices]
                 if not (i == j == k == 0) and vert not in [obj.vertices for obj in cubes]:
-                    cubes.append(Object(vert, pol(), 70))
+                    cubes.append(Object(vert, pol(), 70, size))
 
     rad = 0.05
 
@@ -97,20 +105,16 @@ def main(screen):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
         keys = pygame.key.get_pressed()
         for key in params:
             if keys[key]:
                 for cube in cubes:
                     cube.rotate(*params[key])
-        if keys[pygame.K_w]:
-            for cube in cubes:
-                cube.len += 5
-        if keys[pygame.K_s]:
-            for cube in cubes:
-                cube.len -= 5
         screen.fill(WHITE)
-        for cube in cubes:
-            draw_shape(screen, cube, size)
+        cubes.sort(key=lambda c: average_z(c.vertices))
+
+        draw_shape(screen, cubes)
         pygame.display.flip()
         clock.tick(40)
 
