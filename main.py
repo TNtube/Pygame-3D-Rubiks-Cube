e5d89095fbb86368a205671c5cbca770770f3747
TNtube <stanis.kapus@gmail.com>
2020-11-24 22:06:29 -0300

    now we can move only a face in the right axis

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pygame
 from math import cos, sin
-from random import choice
 
 axis_x, axis_y, axis_z = 0, 1, 2
 
 BLACK, WHITE = 0x000, 0xffffff
 
-colors = [*pygame.color.THECOLORS.values()]
+colors = pygame.color.THECOLORS
 
 
 def mul_matrix(a, b):
@@ -36,15 +35,16 @@ class Object:
     def __init__(self, vertices, edges, length, size):
         self.vertices = vertices
         self.edges = edges
-        self.rotation = [3, 3, 0]
+        self.rotation = [4, 4, 0]
         self.movement = [size[0], size[1], length]
+        self.location = vertices
 
     def rotate(self, axe, o):
         self.rotation[axe] += o
 
     def polygon(self):
-        location = mul_matrix(self.vertices, rot_matrix(*self.rotation))
-        return [(color, (location[v1], location[v2], location[v3], location[v4]))
+        self.location = mul_matrix(self.vertices, rot_matrix(*self.rotation))
+        return [(color, (self.location[v1], self.location[v2], self.location[v3], self.location[v4]))
                 for color, (v1, v2, v3, v4) in self.edges]
 
 
@@ -54,17 +54,20 @@ def point(vec, shape):
 
 
 def average_z(dots):
-    return sum(dot[2] for dot in dots)
+    return sum(dot[2] for dot in dots)/len(dots)
 
 
 def draw_shape(screen, shapes):
     pol = []
     for shape in shapes:
-        pol.extend(shape.polygon())
+        polygon = shape.polygon()
+        pol.extend(polygon)
     pol.sort(key=lambda p: average_z(p[1]))
 
     for col, points in pol:
-        pygame.draw.polygon(screen, col, [*(point(dot, shapes[0])for dot in points)])
+        po = [*(point(dot, shapes[0])for dot in points)]
+        pygame.draw.polygon(screen, col, po)
+        pygame.draw.polygon(screen, BLACK, po, 3)
 
 
 def main(screen):
@@ -79,9 +82,9 @@ def main(screen):
              (0, 4), (1, 5), (2, 6), (3, 7)]
 
     def pol():
-        return [(choice(colors), (0, 1, 3, 2)), (choice(colors), (0, 1, 5, 4)),
-                (choice(colors), (4, 5, 7, 6)), (choice(colors), (2, 3, 7, 6)),
-                (choice(colors), (0, 4, 6, 2)), (choice(colors), (1, 5, 7, 3))]
+        return [(colors["blue"], (0, 1, 3, 2)), (colors["yellow"], (0, 1, 5, 4)),
+                (colors["green"], (4, 5, 7, 6)), (colors["white"], (2, 3, 7, 6)),
+                (colors["orange"], (0, 4, 6, 2)), (colors["red"], (1, 5, 7, 3))]
 
     cubes = []
     for i in range(-1, 2):
@@ -109,10 +112,9 @@ def main(screen):
         keys = pygame.key.get_pressed()
         for key in params:
             if keys[key]:
-                for cube in cubes:
+                for cube in cubes[9:-9]:
                     cube.rotate(*params[key])
         screen.fill(WHITE)
-        cubes.sort(key=lambda c: average_z(c.vertices))
 
         draw_shape(screen, cubes)
         pygame.display.flip()
